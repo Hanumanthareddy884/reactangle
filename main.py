f5dad615f3cf009a071eb0f121d154f96a94c765
Hanumanthareddy <reddyreddy884@gmail.com>
2024-01-15 00:57:54 +0530

    Remove the unwanted import tuple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from typing import List, Tuple
+from typing import List
 
 app = FastAPI()
 
@@ -43,12 +43,14 @@ def find_largest_rectangle(matrix):
                     histogram[j] += 1
                 else:
                     histogram[j] = 0
+
                 current_area = largest_rectangle_area(histogram)
                 if current_area > max_area:
                     max_area = current_area
                     similar_number = num
 
     return similar_number,max_area
+
 @app.post("/largest_rectangle",tags=["Largest Rectangle in Matrix"])
 async def largest_rectangle(matrix: List[List[int]]):
     try:
